253825627daedd2fec9e0cdd1c483aa9d86f125f
KristofferStrube <kristoffer.strube@gmail.com>
2022-11-13 17:03:11 +0100

    Completed task instead of yield

--- samples/KristofferStrube.Blazor.ServiceWorker.WasmExample/Program.cs
+++ samples/KristofferStrube.Blazor.ServiceWorker.WasmExample/Program.cs
@@ -18,21 +18,23 @@ var app = builder.Build();
 
 var navigator = app.Services.GetRequiredService<INavigatorService>();
 var logger = app.Services.GetRequiredService<Logger>();
+
 var serviceWorker = await navigator.GetServiceWorkerAsync();
 await serviceWorker.RegisterAsync("./service-worker.js", async (scope) => {
     scope.OnInstall = async () =>
     {
         logger.WriteLine("We installed!");
-        await Task.Yield();
+        await Task.CompletedTask;
     };
     scope.OnActivate = async () =>
     {
         logger.WriteLine("We activated!");
+        await Task.CompletedTask;
     };
     scope.OnFetch = async (fetchEvent) =>
     {
         logger.WriteLine("We fetched!");
-        await Task.Yield();
+        await Task.CompletedTask;
     };
     scope.OnPush = async (pushEvent) =>
     {
@@ -46,10 +48,10 @@ await serviceWorker.RegisterAsync("./service-worker.js", async (scope) => {
         logger.WriteLine($"json object as string: {await json.AsStringAsync()}");
         var text = await messageData.TextAsync();
         logger.WriteLine($"text: {text}");
-        await Task.Yield();
+        await Task.CompletedTask;
     };
     logger.WriteLine("We Initialized!");
-    await Task.Yield();
+    await Task.CompletedTask;
 });
 
 await app.RunAsync();
